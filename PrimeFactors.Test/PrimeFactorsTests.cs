e6c4e4b2766c50cd043ac8c4d1c47d38482f4b9e
carlosSusarrey <carlos.susarrey.diaz@gmail.com>
2017-01-16 23:59:29 -0600

    complete Kata

---
+++ PrimeFactors.Test/PrimeFactorsTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace PrimeFactors.Test
+{
+    public class PrimeFactorsTests
+    {
+        private static List<int> List(params int[] values)
+        {
+            return values.ToList();
+        }
+
+        [Fact]
+        public void TestOne()
+        {
+            List().ShouldBeEquivalentTo(PrimeFactors.Generate(1));
+        }
+
+        [Fact]
+        public void TestTwo()
+        {
+            List(2).ShouldBeEquivalentTo(PrimeFactors.Generate(2));
+        }
+
+        [Fact]
+        public void TestThree()
+        {
+            List(3).ShouldBeEquivalentTo(PrimeFactors.Generate(3));
+        }
+
+        [Fact]
+        public void TestFour()
+        {
+            List(2, 2).ShouldBeEquivalentTo(PrimeFactors.Generate(4));
+        }
+
+        [Fact]
+        public void TestSix()
+        {
+            List(2, 3).ShouldBeEquivalentTo(PrimeFactors.Generate(6));
+        }
+
+        [Fact]
+        public void TestEight()
+        {
+            List(2,2,2).ShouldBeEquivalentTo(PrimeFactors.Generate(8));
+        }
+
+        [Fact]
+        public void TestNine()
+        {
+            List(3,3).ShouldBeEquivalentTo(PrimeFactors.Generate(9));
+        }
+
+        [Fact]
+        public void TestFourteen()
+        {
+            List(2,7).ShouldBeEquivalentTo(PrimeFactors.Generate(14));
+        }
+
+        [Fact]
+        public void TestFifteen()
+        {
+            List(3,5).ShouldBeEquivalentTo(PrimeFactors.Generate(15));
+        }
+    }
+}
---
+++ PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class PrimeFactors
+    {
+        public static List<int> Generate(int i)
+        {
+            var primes = new List<int>();
+            for (var candidate = 2; i > 1; candidate++)
+            {
+                for (;i % candidate == 0; i /= candidate)
+                {
+                    primes.Add(candidate);
+                }
+            }
+            return primes;
+        }
+    }
+}
